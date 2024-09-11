506ab3cf1401cab19644c30451dbf32d07f892b6
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-09-11 20:21:02 -0300

    form feito

--- Front/index.js
+++ Front/index.js
@@ -1,8 +1,19 @@
+//AO CARREGAR O HTML, CARREGA A DATA ATUAL
 window.onload=function(){
     let dataAtual = new Date();
     let data = dataAtual.getDay() + "/" + dataAtual.getMonth() + "/" + dataAtual.getFullYear();
     document.getElementById("horario-atual").innerHTML = data
 }
+
+//CANCELA E NÃO RECARREGA A PAGINA
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('cancelar');
+    button.addEventListener('click', function(event) {
+        event.preventDefault();
+        let form = document.getElementById("form");
+        form.style.display = "none";
+    });
+});
 
 function abrirModal(){
     let form = document.getElementById("form");
