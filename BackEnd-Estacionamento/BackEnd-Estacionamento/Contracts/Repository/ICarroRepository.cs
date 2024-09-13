8249c5fcf8f0fd786110b198d550397fac38010b
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-09-13 18:47:57 -0300

    Carro DTO feito

---
+++ BackEnd-Estacionamento/BackEnd-Estacionamento/Contracts/Repository/ICarroRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace BackEnd_Estacionamento.Contracts.Repository
+{
+    public interface ICarroRepository
+    {
+        Task Adicionar();
+    }
+}
---
+++ BackEnd-Estacionamento/BackEnd-Estacionamento/DTO/CarroDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Policy;
+
+namespace BackEnd_Estacionamento.DTO
+{
+    public class CarroDTO
+    {
+        public DateTime chegada { get; set; }
+        public DateTime saida { get; set; }
+        public TimeOnly duracao { get; set; }
+        public int tempoCobradoHora { get; set; }
+        public double preco {get; set; }
+        public double valorPagar { get; set; }
+    }
+}
