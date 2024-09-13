34e312f24c77b34bf9e6a8ea8e129d76045180c0
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-09-13 18:50:43 -0300

    CarroEntity feito

---
+++ BackEnd-Estacionamento/BackEnd-Estacionamento/Entity/CarroEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace BackEnd_Estacionamento.Entity
+{
+    public class CarroEntity
+    {
+        public int Id { get; set; } 
+        public DateTime chegada { get; set; }
+        public DateTime saida { get; set; }
+        public TimeOnly duracao { get; set; }
+        public int tempoCobradoHora { get; set; }
+        public double preco { get; set; }
+        public double valorPagar { get; set; }
+    }
+}
