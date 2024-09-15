46f57165b5bd64738ad199fe59889bd6d62221de
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-09-15 12:09:42 -0300

    Correções e melhorias nos botões de pesquisa e cancelar

--- Front/index.js
+++ Front/index.js
@@ -14,37 +14,49 @@ window.onload=function(){
     });
 }
 
-//CANCELA E NÃO RECARREGA A PAGINA
-document.addEventListener('DOMContentLoaded', function() {
-    var button = document.getElementById('cancelar');
-    button.addEventListener('click', function(event) {
-        event.preventDefault();
-        let form = document.getElementById("form");
-        form.style.display = "none";
-    });
-});
-
 //FORMATA A PLACA DIGITADA CORRETAMENTE
 document.addEventListener('DOMContentLoaded', function() {
-    let placa = document.getElementById('placa'); 
-    placa.addEventListener('input', function(event) {
-        let value = placa.value;
-        value = value.replace(/[^A-Z0-9]/gi, '');
-        if (value.length <= 7) {
-            value = value.toUpperCase(); 
-            value = value.replace(/^([A-Z]{3})([A-Z0-9]{0,4})/, '$1-$2');
-        }    
-        placa.value = value;
-    });
+    let placas = document.getElementsByClassName('input-placa'); 
+    for(const placaInd of placas){
+        placaInd.addEventListener('input', function(event) {
+            let value = placaInd.value;
+            value = value.replace(/[^A-Z0-9]/gi, '');
+            if (value.length <= 7) {
+                value = value.toUpperCase(); 
+                value = value.replace(/^([A-Z]{3})([A-Z0-9]{0,4})/, '$1-$2');
+            }    
+            placaInd.value = value;
+        });
+    }
 });
 
-//ABRE E FECHA O MODAL DE ENVIO DA PLACA DO CARRO
+//ABRE O MODAL DE ENVIO DA PLACA DO CARRO
 function abrirModal(){
     let input = document.getElementById("placa");
     input.value = "";
+    cancelar();
     let form = document.getElementById("form");
-    form.style.display = "block";
-    
+    form.style.display = "block"; 
+}
+
+function abrirModalPesquisa(){
+    let input = document.getElementById("placa");
+    input.value = "";
+    cancelar();
+    let divPesquisa = document.getElementById("caixaPesquisa");
+    divPesquisa.style.display = "block";
+}
+
+function cancelar(){
+    event.preventDefault();
+    let input = document.getElementsByClassName("input-placa");
+    for(const inputSeparado of input){
+        inputSeparado.value = "";
+    }
+    let divs = document.getElementsByClassName("modal-meu")
+    for(const divsSeparadas of divs){
+        divsSeparadas.style.display = "none";
+    }  
 }
 
 //ENVIO DA PLACA PARA O BANCO
